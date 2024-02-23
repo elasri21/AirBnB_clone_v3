42b9df365e1c7c0adc7d7c32543d704a69e4d85e
elasri21 <islame.edu@gmail.com>
2024-02-23 08:38:56 -0800

    ts

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,12 @@
 """Contains app, instance of Flask"""
 
 from flask import Flask, jsonify
-# from flask_cors import CORS
+from flask_cors import CORS
 from os import getenv
 from api.v1.views import app_views
 from models import storage
 app = Flask(__name__)
-# CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
@@ -18,4 +18,4 @@ def teardown(exception):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000", threaded=True)
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
